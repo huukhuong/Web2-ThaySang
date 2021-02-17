435e2d33108c0ef5fa5b563daf859916da7b9466
tranhuukhuong1 <65057710+tranhuukhuong1@users.noreply.github.com>
2021-02-17 22:11:21 +0700

    base sơ bộ

--- footer.php
+++ footer.php
@@ -1,64 +1,94 @@
 <!-- FOOTER -->
-
-<footer class="page-footer font-small footer-content">
-    <div class="container text-center text-md-left">
+<footer class="footer">
+    <div class="container">
         <div class="row">
-            <div class="footer-logo col-md-6 mt-md-0 mt-3 text-center">
-                <a href="./index.html" style="text-decoration: none;">
-                    <img src="./img/logo.png" alt="" class="footer-img">
-                </a>
-                <div class="footer-address">
-                    Địa chỉ: 273 An Dương Vương, Phường 3, Quận 5, Thành phố Hồ Chí Minh <br>
-                    Tổng đài hỗ trợ: 1900.1221 - Email: cskh@hotro.myweb.vn <br>
-                    Mã số doanh nghiệp: 0232733791 do Sở Kế hoạch & Đầu tư TP Hồ Chí Minh <br>
-                    cấp lần đầu ngày 12/02/2021
-                </div>
-            </div>
-            <hr class="clearfix w-100 d-md-none pb-3">
-            <div class="col-md-2 mb-md-0 mb-3">
-                <h3 class="text-uppercase">Giới thiệu</h3>
-                <ul class="footer-list list-unstyled">
-                    <li>
-                        <a data-toggle="modal" data-target="#gioiThieu" href="">Về chúng tôi</a>
+            <div class="col-md-3 col-6">
+                <h3 class="footer__heading">Chăm sóc khách hàng</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Trung tâm trợ giúp</a>
                     </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">MyShop Mall</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Hướng dẫn mua hàng</a>
+                    </li>
+                </ul>
             </div>
-            <div class="col-md-2 mb-md-0 mb-3">
-                <h3 class="text-uppercase">Liên hệ</h3>
-                <ul class="footer-list list-unstyled">
-                    <li>
-                        <a href="https://www.facebook.com.vn">
-                            <span class="footer-icon">
-                                <i class="fab fa-facebook-square"></i>
-                            </span>
+            <div class="col-md-3 col-6">
+                <h3 class="footer__heading">Giới thiệu shop</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Giới thiệu về MyShop</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Tuyển dụng</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Điều khoản MyShop</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-6">
+                <h3 class="footer__heading">Danh mục</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Thời trang</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Thiết bị điện tử</a>
+                    </li>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">Bách hoá online</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-6">
+                <h3 class="footer__heading">Theo dõi</h3>
+                <ul class="footer-list">
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">
+                            <i class="footer-item__icon1 fab fa-facebook"></i>
                             Facebook
                         </a>
                     </li>
-                    <li>
-                        <a href="https://instagram.com.vn">
-                            <span class="footer-icon">
-                                <i class="fab fa-instagram-square"></i>
-                            </span>
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">
+                            <i class="footer-item__icon fab fa-instagram"></i>
                             Instagram
                         </a>
                     </li>
-                    <li>
-                        <a href="https://www.youtube.com.vn">
-                            <span class="footer-icon">
-                                <i class="fab fa-youtube"></i>
-                            </span>
-                            Youtube
+                    <li class="footer-item">
+                        <a href="#" class="footer-item__link">
+                            <i class="footer-item__icon fab fa-linkedin"></i>
+                            LinkedIn
                         </a>
                     </li>
                 </ul>
             </div>
         </div>
     </div>
-    <div class="footer-lincense footer-copyright text-center py-3">
-        Bản quyền &copy; 2021 SGU. Đã đăng ký bản quyền. <br>
-        Hoạt động tốt nhất trên GoogleChrome.
+    <div class="footer__bottom">
+        <div class="container">
+            <div class="text-center">
+                <p class="footer__text">Công ty TNHH MyShop</p>
+                <img src="./img/lincense1.png" alt="" class="footer__bottom-lincense-img">
+                <img src="./img/lincense2.png" alt="" class="footer__bottom-lincense-img">
+                <img src="./img/lincense3.png" alt="" class="footer__bottom-lincense-img">
+            </div>
+            <p></p>
+            <p class="footer__text">Địa chỉ: 273 An Dương Vương, Phường 3, Quận 5, Thành phố Hồ Chí Minh.</p>
+            <p class="footer__text">Tổng đài hỗ trợ: 19001221 - Email: cskh@hotro.shopsgu.vn</p>
+            <p class="footer__text">Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch & Đầu tư TP Hồ Chí Minh cấp lần đầu ngày 10/02/2021</p>
+        </div>
+    </div>
+    <div class="footer__down">
+        <p class="footer__text-lincense">&copy; 2021 - Bản quyền thuộc về MyShop</p>
     </div>
 </footer>
-<!-- END FOOOTER -->
+</div>
+
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
