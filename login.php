c3643765256b1b89900ffe838c9adb93a698c863
tranhuukhuong1 <65057710+tranhuukhuong1@users.noreply.github.com>
2021-02-18 16:17:49 +0700

    thêm giao diện và validate form

--- login.php
+++ login.php
@@ -32,25 +32,30 @@ include("./header_login_register.php");
 ?>
 
 <body>
-    <div id="login pb-10">
-        <h3 class="text-center text-white pt-5">Đăng nhập</h3>
+    <div class="container-fluid">
         <div class="container">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-md-6 mb-5">
-                    <div class="col-md-12">
-                        <form name="myForm" method="post" action="" onsubmit="return validateForm()">
-                            <div class="form-group">
-                                <label for="email">Địa chỉ email</label>
-                                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Mật khẩu</label>
-                                <input type="password" class="form-control" id="password" name="password">
-                            </div>
-                            <button type="submit" class="btn btn-primary" name="btn_submit">Đăng nhập</button>
-                            <p><a href="#">Quên mật khẩu?</a></p>
-                            <p>Chưa có tài khoản? <a href="./register.php">Đăng ký ngay</a></p>
-                        </form>
+            <div class="row text-center">
+                <div class="login-wrap my-5 col">
+                    <h3 class="text-center pt-5 my-0">Đăng nhập</h3>
+                    <div class="row text-center">
+                        <div class="col">
+                            <form name="myForm" method="post" action="" onsubmit="return validateForm()">
+                                <div class="form-group text-left">
+                                    <input type="email" class="form-control" id="email" name="email" autofocus autocomplete="on" placeholder="Địa chỉ email" aria-describedby="emailHelp">
+                                    <p class="message_err" id="err_email">&nbsp;</p>
+                                </div>
+                                <div class="form-group text-left">
+                                    <input type="password" class="form-control" id="password" name="password" autocomplete="on" placeholder="Mật khẩu">
+                                    <p class="message_err" id="err_password">&nbsp;</p>
+                                    <a class="log-link" href="#">Quên mật khẩu?</a>
+                                </div>
+                                <button type="submit" class="btn btn_login" name="btn_submit">Đăng nhập</button>
+                                <p class="pt-4">
+                                    Lần đầu truy cập?
+                                    <a class="log-link" href="./register.php?goto=register">Đăng ký tài khoản</a>
+                                </p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -63,14 +68,21 @@ include("./header_login_register.php");
 
     <script>
         function validateForm() {
-            let x = document.forms["myForm"]["email"].value;
-            if (x == "") {
-                alert("Email không được để trống!");
-                return false;
-            }
-            x = document.forms["myForm"]["password"].value;
-            if (x == "") {
-                alert("Mật khẩu không được để trống!");
+            let x = document.getElementById("email");
+            let y = document.getElementById("password");
+            if (x.value == "" || y.value == "") {
+                let err_mail = document.getElementById('err_email');
+                let err_pass = document.getElementById('err_password');
+
+                err_mail.innerText = 'Không được để trống trường này';
+                err_pass.innerText = 'Mật khẩu phải có ít nhất 8 ký tự';
+
+                if (err_mail.innerText != "") {
+                    x.style.border = "1px solid red";
+                }
+                if (err_pass.innerText != "") {
+                    y.style.border = "1px solid red";
+                }
                 return false;
             }
         }
