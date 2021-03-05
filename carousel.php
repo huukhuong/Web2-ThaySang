fe6123c9036861701c62dd409b506d74cf9e1018
huukhuong <huukhuong.it@gmail.com>
2021-03-05 14:41:43 +0700

    Animation caption carousel, responsive carousel

--- carousel.php
+++ carousel.php
@@ -1,5 +1,5 @@
 <!-- CAROUSEL -->
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3500">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="4000">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -8,12 +8,27 @@
     <div class="carousel-inner">
         <div class="carousel-item active">
             <img src="./img/banner0.jpg" class="d-block" alt="banner 0">
+            <div class="carousel-caption d-block">
+                <h5 class="animate__animated animate__bounceInRight">Thức ăn thú cưng</h5>
+                <p class="animate__animated animate__bounceInLeft">Thức ăn đầy đủ các loại, đảm bảo dinh dưỡng.</p>
+                <p class="animate__animated animate__bounceInRight"><a href="">Xem thêm</a></p>
+            </div>
         </div>
         <div class="carousel-item">
-            <img src="./img/banner1.jpg" class="d-block" alt="banner 1">
+            <img src="./img/banner1.jpeg" class="d-block" alt="banner 2">
+            <div class="carousel-caption d-block">
+                <h5 class="animate__animated animate__zoomIn">Chăm sóc thú cưng</h5>
+                <p class="animate__animated animate__slideInUp">Bao gồm các dịch vụ tắm, tỉa lông, cắt móng,...</p>
+                <p class="animate__animated animate__backInRight"><a href="">Xem thêm</a></p>
+            </div>
         </div>
         <div class="carousel-item">
-            <img src="./img/banner2.jpg" class="d-block" alt="banner 2">
+            <img src="./img/banner2.jpeg" class="d-block" alt="banner 1">
+            <div class="carousel-caption d-block">
+                <h5 class="animate__animated animate__fadeInDown">Dịch vụ thú y</h5>
+                <p class="animate__animated animate__fadeInUp">Chăm lo cho sức khỏe thú cưng của bạn.<br> Với đội ngũ bác sĩ thú y tận tâm, giàu kinh nghiệm.</p>
+                <p class="animate__animated animate__flipInY"><a href="">Xem thêm</a></p>
+            </div>
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
--- header.php
+++ header.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- jQuery library -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Animation libary -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <!-- Custom CSS file -->
     <link rel="stylesheet" href="./css/main.css">
 </head>
